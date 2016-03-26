52b63072e4c5dc7abbdb1cc05ebe6ca7a1d2a48c
Marc King <mjking@umich.edu>
2016-03-26 14:08:47 -0400

    Initial SQLite setup. Still needs the database tables to be created.

--- Ice Cream Manager/Application/Properties/Settings.Designer.cs
+++ Ice Cream Manager/Application/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace IceCreamManager.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("data source=\"C:\\Cosmosys\\Ice Cream Manager\\Application\\bin\\Debug\\IceCreamManager." +
+            "db3\"")]
+        public string IceCreamManagerConnectionString {
+            get {
+                return ((string)(this["IceCreamManagerConnectionString"]));
+            }
+        }
     }
 }
