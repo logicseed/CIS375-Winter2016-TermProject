3712129d8162155958099a41c0d0a5592cde4f3b
Marc King <mjking@umich.edu>
2016-03-27 11:18:26 -0400

    Created DualListPanel

---
+++ Ice Cream Manager/Application/View/DualListPanel.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace IceCreamManager.View
+{
+    partial class DualListPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dualListPanelSplitContainer = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.dualListPanelSplitContainer)).BeginInit();
+            this.dualListPanelSplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dualListPanelSplitContainer
+            // 
+            this.dualListPanelSplitContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dualListPanelSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dualListPanelSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.dualListPanelSplitContainer.Name = "dualListPanelSplitContainer";
+            this.dualListPanelSplitContainer.Size = new System.Drawing.Size(800, 600);
+            this.dualListPanelSplitContainer.SplitterDistance = 500;
+            this.dualListPanelSplitContainer.SplitterWidth = 2;
+            this.dualListPanelSplitContainer.TabIndex = 0;
+            // 
+            // DualListPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dualListPanelSplitContainer);
+            this.Name = "DualListPanel";
+            this.Size = new System.Drawing.Size(800, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.dualListPanelSplitContainer)).EndInit();
+            this.dualListPanelSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer dualListPanelSplitContainer;
+    }
+}
---
+++ Ice Cream Manager/Application/View/DualListPanel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IceCreamManager.View
+{
+    public partial class DualListPanel : UserControl
+    {
+        public DualListPanel()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Adds a Control to the specified Panel in the DualListPanel SplitContainer.
+        /// </summary>
+        /// <param name="control">Control to add.</param>
+        /// <param name="panel">Panel number of SplitContainer to add the Control to</param>
+        public void Add(Control control, int panel)
+        {
+            if (panel == 1)
+            {
+                foreach (Control oldControl in this.dualListPanelSplitContainer.Panel1.Controls)
+                {
+                    this.dualListPanelSplitContainer.Panel1.Controls.Remove(oldControl);
+                }
+                this.dualListPanelSplitContainer.Panel1.Controls.Add(control);
+            }
+            else if (panel == 2)
+            {
+                foreach (Control oldControl in this.dualListPanelSplitContainer.Panel2.Controls)
+                {
+                    this.dualListPanelSplitContainer.Panel2.Controls.Remove(oldControl);
+                }
+                this.dualListPanelSplitContainer.Panel2.Controls.Add(control);
+            }
+        }
+    }
+}
