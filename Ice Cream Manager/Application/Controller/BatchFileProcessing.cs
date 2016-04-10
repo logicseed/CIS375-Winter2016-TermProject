a5f7a5233b6a930e2b3e82af2b1790c4342d074f
Rodney Lewis <rodneyll@umich.edu>
2016-04-09 20:43:25 -0400

    Initial set up for BatchFileProcessing and it's related tests.

---
+++ Ice Cream Manager/Application/Controller/BatchFileProcessing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using IceCreamManager.Model;
+
+namespace IceCreamManager.Controller
+{
+    class BatchFileProcessing
+    {
+        DatabaseManager Database = DatabaseManager.DatabaseReference;
+        private string fileLine;
+        private string temp;
+        private string[] recordTypes = { "IR", "TR", "SR", "T " };
+
+        public void processHeaderFooter(string FilePath, int ID)
+        {
+
+        }
+    }
+}
+
