64ef8e93f8767ce93b8cdcc64f90e68dbd60f530
Marc King <mjking@umich.edu>
2016-04-10 15:24:27 -0400

    Initial ActionTypes and ActionSources

---
+++ Ice Cream Manager/Application/Model/ActionSource.cs
@@ -0,0 +1,13 @@
+﻿/// <project> IceCreamManager </project>
+/// <module> ActionSource </module>
+/// <author> Marc King </author>
+/// <date_created> 2016-04-10 </date_created>
+
+namespace IceCreamManager.Model
+{
+    public enum ActionSource
+    {
+        BatchFile,
+        UserInterface
+    }
+}
---
+++ Ice Cream Manager/Application/Model/ActionType.cs
@@ -0,0 +1,14 @@
+﻿/// <project> IceCreamManager </project>
+/// <module> ActionType </module>
+/// <author> Marc King </author>
+/// <date_created> 2016-04-10 </date_created>
+
+namespace IceCreamManager.Model
+{
+    public enum ActionType
+    {
+        AddCityToRoute,
+        AddTruckToRoute,
+        AddItemToInventory,
+    }
+}
