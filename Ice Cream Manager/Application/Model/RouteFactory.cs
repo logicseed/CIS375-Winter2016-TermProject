2f1e95e07578c256fcc0bb89b87563136bcab9bd
Marc King <mjking@umich.edu>
2016-04-10 20:51:16 -0400

    Added RouteFactory.

---
+++ Ice Cream Manager/Application/Model/RouteFactory.cs
@@ -0,0 +1,22 @@
+﻿/// <project> IceCreamManager </project>
+/// <module> RouteFactory </module>
+/// <author> Marc King </author>
+/// <date_created> 2016-04-10 </date_created>
+
+namespace IceCreamManager.Model
+{
+    public static class RouteFactory
+    {
+        private static DatabaseEntityFactory<Route> DatabaseRouteFactory = new DatabaseEntityFactory<Route>();
+
+        public static Route Load(int ID)
+        {
+            return DatabaseRouteFactory.Load(ID);
+        }
+
+        public static Route Create(RouteProperties EntityProperties)
+        {
+            return DatabaseRouteFactory.Create(EntityProperties);
+        }
+    }
+}
