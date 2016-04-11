c4f80027b4360a63f8147f26c967717526cbb688
Marc King <mjking@umich.edu>
2016-04-11 11:22:43 -0400

    Comments added where needed.

--- Ice Cream Manager/Application/Model/Item.cs
+++ Ice Cream Manager/Application/Model/Item.cs
@@ -9,6 +9,13 @@ using System.Data;
 namespace IceCreamManager.Model
 {
     // COMMENT: This file needs comments.
+
+    /// <summary>
+    ///   Contains the properties of an Item. 
+    /// </summary>
+    /// <remarks>
+    ///   A class was chosen over struct because of how struct will be boxed when passing as the implemented type.
+    /// </remarks>
     public class ItemProperties : DatabaseEntityProperties
     {
         public int Number;
@@ -18,6 +25,9 @@ namespace IceCreamManager.Model
         public int Quantity;
     }
 
+    /// <summary>
+    ///   Represents a type of ice cream sold by ice cream trucks. 
+    /// </summary>
     public class Item : DatabaseEntity
     {
         private int number;
@@ -36,6 +46,9 @@ namespace IceCreamManager.Model
             Load(ID);
         }
 
+        /// <summary>
+        ///   User provided number to distinguish the item. 
+        /// </summary>
         public int Number
         {
             get
@@ -55,6 +68,9 @@ namespace IceCreamManager.Model
             }
         }
 
+        /// <summary>
+        ///   User provided description of the item. 
+        /// </summary>
         public string Description
         {
             get
@@ -74,6 +90,9 @@ namespace IceCreamManager.Model
             }
         }
 
+        /// <summary>
+        ///   The price of the item used in calculating sales and waste. 
+        /// </summary>
         public double Price
         {
             get
@@ -93,6 +112,10 @@ namespace IceCreamManager.Model
             }
         }
 
+        /// <summary>
+        ///   How many days an item will last after being created. An item is considered to have been created when it is
+        ///   placed on a truck.
+        /// </summary>
         public int Lifetime
         {
             get
@@ -112,6 +135,9 @@ namespace IceCreamManager.Model
             }
         }
 
+        /// <summary>
+        ///   The warehouse quantity of this ice type. 
+        /// </summary>
         public int Quantity
         {
             get
@@ -127,14 +153,28 @@ namespace IceCreamManager.Model
             }
         }
 
+        /// <summary>
+        ///   The naem of the database table that stores items. 
+        /// </summary>
         protected override string TableName => "item";
 
+        /// <summary>
+        ///   The SQL command used to update an item in the database with this object's properties. 
+        /// </summary>
         protected override string UpdateCommand =>
             $"UPDATE {TableName} SET (number,description,price,lifetime,quantity) VALUES ({Number},'{Description}',{Price},{Lifetime},{Quantity}) WHERE id = {ID}";
 
+        /// <summary>
+        ///   The SQL command used to create an item in the database with this object's properties. 
+        /// </summary>
         protected override string CreateCommand =>
             $"INSERT INTO {TableName} (number,description,price,lifetime,quantity) VALUES ({Number},'{Description}',{Price},{Lifetime},{Quantity})";
 
+        /// <summary>
+        ///   Load an item from the database based on the provided identity. 
+        /// </summary>
+        /// <param name="ID"> The unique item identity. </param>
+        /// <returns> Whether or not the item was successfully loaded. </returns>
         public override bool Load(int ID)
         {
             this.ID = ID;
@@ -152,6 +192,11 @@ namespace IceCreamManager.Model
             return true;
         }
 
+        /// <summary>
+        ///   Fills this item's properties with values. 
+        /// </summary>
+        /// <param name="EntityProperties"> A DatabaseEntityProperties object with the values to use. </param>
+        /// <returns> Whether or not the item was successfully filled with the values. </returns>
         public override bool Fill(DatabaseEntityProperties EntityProperties)
         {
             ItemProperties ItemValues = EntityProperties as ItemProperties;
