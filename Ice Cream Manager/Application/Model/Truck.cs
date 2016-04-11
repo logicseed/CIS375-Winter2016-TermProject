232d1afab4ae07c17ac225e784239411d1045e94
Marc King <mjking@umich.edu>
2016-04-10 20:57:45 -0400

    Initial Truck and TruckFactory layout, commiting because Inventory needs to be complete first.

---
+++ Ice Cream Manager/Application/Model/Truck.cs
@@ -0,0 +1,50 @@
+﻿/// <project> IceCreamManager </project>
+/// <module> Truck </module>
+/// <author> Marc King </author>
+/// <date_created> 2016-04-10 </date_created>
+
+using System;
+
+namespace IceCreamManager.Model
+{
+    public class TruckProperties : DatabaseEntityProperties
+    {
+    }
+
+    public class Truck : DatabaseEntity
+    {
+        protected override string CreateCommand
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override string TableName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override string UpdateCommand
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool Fill(DatabaseEntityProperties EntityProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Load(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ice Cream Manager/Application/Model/TruckFactory.cs
@@ -0,0 +1,11 @@
+﻿/// <project> IceCreamManager </project>
+/// <module> TruckFactory </module>
+/// <author> Marc King </author>
+/// <date_created> 2016-04-10 </date_created>
+
+namespace IceCreamManager.Model
+{
+    public class TruckFactory
+    {
+    }
+}
