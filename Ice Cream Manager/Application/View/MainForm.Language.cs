55a6239aa44e1227749e2bd3735365c5e29d33fc
Marc King <mjking@umich.edu>
2016-04-25 10:23:51 -0400

    Initial recreation of MainForm.Language.cs, needs LanguageManager to be recreated to be completed.

--- Ice Cream Manager/Application/View/MainForm.Language.cs
+++ Ice Cream Manager/Application/View/MainForm.Language.cs
@@ -3,39 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IceCreamManager.Model;
 
 namespace IceCreamManager.View
 {
     partial class MainForm
     {
+
         private void LocalizeForm(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            Text = "!Ice Cream Manager";
+            RevenueTab.Text = "!Revenue";
+            TrucksTab.Text = "!Trucks";
+            ItemsTab.Text = "!Items";
+            DriversTab.Text = "!Drivers";
+            RoutesTab.Text = "!Routes";
+            CitiesTab.Text = "!Cities";
+            StatusLabel.Text = "!Processing...";
+            MainToolTips.SetToolTip(this.SettingsButton, "!Settings");
+            MainToolTips.SetToolTip(this.LogButton, "!View Log");
+            MainToolTips.SetToolTip(this.AboutButton, "!About");
         }
 
         private void SetLocalizedCityStrings()
         {
-            throw new NotImplementedException();
+            AddCityButton.Text = "!Add";
+            EditCityButton.Text = "!Edit";
+            RemoveCityButton.Text = "!Remove";
+            ShowDeletedCities.Text = "!Show Deleted";
+
         }
 
         private void SetLocalizedDriverStrings()
         {
-            throw new NotImplementedException();
+            AddDriverButton.Text = "!Add";
+            EditDriverButton.Text = "!Edit";
+            RemoveDriverButton.Text = "!Remove";
+            ShowDeletedDrivers.Text = "!Show Deleted";
         }
 
         private void SetLocalizedItemStrings()
         {
-            throw new NotImplementedException();
+            AddItemButton.Text = "!Add";
+            EditItemButton.Text = "!Edit";
+            RemoveItemButton.Text = "!Remove";
+            ShowDeletedItems.Text = "!Show Deleted";
         }
 
         private void SetLocalizedRouteStrings()
         {
-            throw new NotImplementedException();
+            AddRouteButton.Text = "!Add";
+            EditRouteButton.Text = "!Edit";
+            RemoveRouteButton.Text = "!Remove";
+            ShowDeletedRoutes.Text = "!Show Deleted";
         }
 
         private void SetLocalizedTruckStrings()
         {
-            throw new NotImplementedException();
+            AddTruckButton.Text = "!Add";
+            EditTruckButton.Text = "!Edit";
+            RemoveTruckButton.Text = "!Remove";
+            DefaultInventoryButton.Text = "!Default Inventory";
+            ShowDeletedTrucks.Text = "!Show Deleted";
+        }
+
+        private void SetLocalizedRevenueStrings()
+        {
+            StartDateLabel.Text = "!Start Date";
+            EndDateLabel.Text = "!End Date";
+            RouteRevenueLabel.Text = "!Route";
+            CityRevenueLabel.Text = "!City";
+            TruckRevenueLabel.Text = "!Truck";
+            DriverRevenueLabel.Text = "!Driver";
+            ItemRevenueLabel.Text = "!Item";
+        }
+
+        private void SetLocalizedBatchStrings()
+        {
+            BatchTab.Text = "!Batch";
         }
     }
 }
